923969aa05ee02a9439c6994ffb1c124fee1583b
Zaid Garcia <viczaidr@gmail.com>
2025-04-04 16:01:56 -0600

    Excluiones

--- RSQR/obj/Debug/net8.0/RSQR.AssemblyInfo.cs
+++ RSQR/obj/Debug/net8.0/RSQR.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RSQR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b81a90b3dcbf4b2bc86cb068028d96db7bb717b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa8c1ee3294c08c29e70046a607a9fe629f6c32")]
 [assembly: System.Reflection.AssemblyProductAttribute("RSQR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RSQR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
